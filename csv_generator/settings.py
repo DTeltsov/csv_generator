8fc8d487478f661671911b1dddef59a4c7575b0d
DTeltsov <63288770+DTeltsov@users.noreply.github.com>
2022-04-27 11:13:28 +0300

    Update settings.py

--- csv_generator/settings.py
+++ csv_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1&om6b^jeg*mct9g2pg$_q@oeho==g+b274+r6do@4=&db=%eb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
